190df4af494921f4d9bb8eb3143f45255799a9dd
Konstantin Morozov <konstantin.morozov@openmedia.co>
2021-03-13 10:38:03 +0300

    fix inner class

--- src/main/java/com/goodok/service/IService.java
+++ src/main/java/com/goodok/service/IService.java
@@ -1,6 +1,9 @@
 package com.goodok.service;
 
+import java.net.Socket;
+
 public interface IService {
     void init(int port);
     void run();
+    void notifyClients(Socket fromClient, String msg);
 }
